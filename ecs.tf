ac401deccdbe14462f889a52ffa74c6ebfb25176
sharanu67 <sharanurathod6677@gmail.com>
2025-04-08 08:42:47 +0530

    Updated ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -26,3 +26,25 @@ resource "aws_ecs_task_definition" "medusa_task" {
     }
   ])
 }
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.medusa_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.medusa_tg.arn
+    container_name   = "medusa"
+    container_port   = 9000
+  }
+
+  depends_on = [aws_lb_listener.frontend]
+}
