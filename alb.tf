e47db14649b22c1075acff2ea8346c4eebc0103e
sharanu67 <sharanurathod6677@gmail.com>
2025-04-08 08:37:14 +0530

    Updated alb.tf

--- alb.tf
+++ alb.tf
@@ -1,30 +1,71 @@
+resource "aws_security_group" "medusa_sg" {
+  name        = "medusa-sg"
+  description = "Allow HTTP and Medusa access"
+  vpc_id      = aws_vpc.medusa_vpc.id
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_lb" "medusa_alb" {
   name               = "medusa-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.medusa_sg.id]
-  subnets            = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
-
 
 resource "aws_lb_target_group" "medusa_tg" {
   name     = "medusa-tg"
   port     = 9000
   protocol = "HTTP"
   vpc_id   = aws_vpc.medusa_vpc.id
+
+  health_check {
+    path                = "/store/products"
+    port                = "9000"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "frontend" {
   load_balancer_arn = aws_lb.medusa_alb.arn
-  port              = "80"
+  port              = 9000
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.medusa_tg.arn
   }
 }
+
+
+
+
+
+
+
+
