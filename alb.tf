f502f1da6a09462a380675aaf91b09651cb3382e
sharanu67 <sharanurathod6677@gmail.com>
2025-04-08 08:13:45 +0530

    Update alb.tf files

--- alb.tf
+++ alb.tf
@@ -1,10 +1,15 @@
+
 resource "aws_lb" "medusa_alb" {
   name               = "medusa-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.medusa_sg.id]
-  subnets           = [aws_subnet.public_subnet.id]
+  subnets            = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
 }
+
 
 resource "aws_lb_target_group" "medusa_tg" {
   name     = "medusa-tg"
