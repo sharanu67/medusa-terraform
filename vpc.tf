a52c4371183d265fa72475b613fe4fd00d1b0b5e
sharanu67 <sharanurathod6677@gmail.com>
2025-04-08 08:12:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,24 @@ resource "aws_vpc" "medusa_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_vpc" "medusa_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.medusa_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 }
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.medusa_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+}
+
 
 resource "aws_security_group" "medusa_sg" {
   vpc_id = aws_vpc.medusa_vpc.id
@@ -33,3 +45,4 @@ resource "aws_security_group" "medusa_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
